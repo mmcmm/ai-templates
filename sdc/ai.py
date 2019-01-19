ac8efc639a7a4c59eadfe2e83d7536bca9c84efc
mtdx <ms2asdk@gmail.com>
2019-01-19 18:21:21 +0200

    init method

--- sdc/ai.py
+++ sdc/ai.py
@@ -1,9 +1,21 @@
+# AI for self driving car
+
 import numpy as np
-import random  # for experience replay
-import os  # to load/save the model
-import torch  # pytorch for dynamic graphics
-import torch.nn  # neural network
+import random
+import os
+import torch
+import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim  # optimizer for stochastic gradient descent
-import torch.autograd as autograd  # tensor to variables
+import torch.optim as optim
+import torch.autograd as autograd
 from torch.autograd import Variable
+
+# creating the architecture  of the neural network
+class Network(nn.Module):
+
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
