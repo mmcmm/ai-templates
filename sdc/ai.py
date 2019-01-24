a27e3063ec4e129a208a28190733ac67a16c3de8
mtdx <ms2asdk@gmail.com>
2019-01-24 15:20:51 +0200

    soft max method

--- sdc/ai.py
+++ sdc/ai.py
@@ -57,3 +57,8 @@ class Dgn():
         self.last_state = torch.Tensor(input_size).unsqueeze(0)
         self.last_action = 0
         self.last_reward = 0
+    
+    def select_action(self, state):
+        probs = F.softmax(self.model(Variable(state, volatile = True)*7)) # T = 7
+        action = probs.multinomial()
+        return action.data[0, 0]
