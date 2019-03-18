ad2f49c7dcf9e36d8173ddec5a99aa5ce2d32d6c
mtdx <ms2asdk@gmail.com>
2019-03-18 20:56:02 +0200

    add som base

---
+++ nn/som/som.py
@@ -0,0 +1,16 @@
+# Self Organizing Map
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('dataset.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
