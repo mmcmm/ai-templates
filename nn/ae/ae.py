8511727ddba454a01695898002f41af2b8a5fabe
mtdx <ms2asdk@gmail.com>
2019-05-13 16:24:38 +0300

    preparing the data

--- nn/ae/ae.py
+++ nn/ae/ae.py
@@ -18,3 +18,34 @@ users = pd.read_csv('datasets/users.dat', sep='::', header=None,
 ratings = pd.read_csv('datasets/ratings.dat', sep='::',
                       header=None, engine='python', encoding='latin-1')
 
+# Preparing the training set and the test set
+training_set = pd.read_csv('datasets/u1.base', delimiter='\t')
+training_set = np.array(training_set, dtype='int')
+test_set = pd.read_csv('datasets/u1.test', delimiter='\t')
+test_set = np.array(test_set, dtype='int')
+
+# Getting the number of users and movies
+nb_users = int(max(max(training_set[:, 0]), max(test_set[:, 0])))
+nb_movies = int(max(max(training_set[:, 1]), max(test_set[:, 1])))
+
+# Converting the data into an array with users in lines and movies in columns
+
+
+def convert(data):
+    new_data = []
+    for id_users in range(1, nb_users + 1):
+        id_movies = data[:, 1][data[:, 0] == id_users]
+        id_ratings = data[:, 2][data[:, 0] == id_users]
+        ratings = np.zeros(nb_movies)
+        ratings[id_movies - 1] = id_ratings
+        new_data.append(list(ratings))
+    return new_data
+
+
+training_set = convert(training_set)
+test_set = convert(test_set)
+
+# Converting the data into Torch tensors
+training_set = torch.FloatTensor(training_set)
+test_set = torch.FloatTensor(test_set)
+
